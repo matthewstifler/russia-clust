4f9672e0a46a4b4e833856a7bd688322220cb2e6
mishaulitskiy <mtwstflr@gmail.com>
2016-12-11 02:55:15 +0000

    Added getting offsetted eview links

--- running-script.R
+++ running-script.R
@@ -6,13 +6,23 @@ cityIds = c(298536, 298484, 298520, 298540, 298529, 298500, 298532, 1380981, 608
 #Getting hotel links
 links = lapply(setNames(cityIds, cityIds), function(cityId){ #setNames is used to have named list; see http://stackoverflow.com/a/18520422/3818255
   getTotalOffset(cityId) %>% 
-    getHotelListLinks(cityId) %>% 
+    getOffsetLinks(cityId) %>% 
     lapply(function(url) getHotelLinksByPage(url, cityId)) %>%
     unlist()
 })
 
 #how many hotels we fetched
 sapply(links, length) %>% sum
+
+#1.
+start <- Sys.time()
+links.reviews <- lapply(links, function(city) {
+  lapply(city, function(link) {
+    getTotalOffsetByUrl(link, is.hotel = T) %>%
+            getOffsetLinksByUrl(link, is.hotel = T)
+  })
+})
+Sys.time() - start
 
 #roadmap for following work:
 #1. Generate a list of all hotel review links with needed offsets of the look city[hotel[review-link-1, review-link-2, ...]]
